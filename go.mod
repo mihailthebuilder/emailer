24c593c33fa84ee33c38b3b1691c426414a3fafc
Mihail Marian <mihailmarian12@gmail.com>
2023-04-13 11:13:25 +0100

    wip

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module emailer
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if isRunningLocally() {
+		loadEnvironmentVariablesFromDotEnvFile()
+	}
+
+	runApplication()
+}
+
+func isRunningLocally() bool {
+	return os.Getenv("GIN_MODE") == ""
+}
+
+func loadEnvironmentVariablesFromDotEnvFile() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
